be509b6b26276c6c4290aa6622a89b4b5f1d1735
VM Sergiu <44142802+srmedan@users.noreply.github.com>
2019-12-27 11:14:38 +0100

    initial setup

---
+++ destination_chooser.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 10:18:50 2019
+
+@author: smedan
+"""
+import operator
+
+#dictionary with the ranks for each country
+destinations_user1 = {
+        'Italy': 5,
+        'Romania': 1, 
+        'Germany': 3
+
+        }
+
+destinations_user2 = {
+        'South Africa': 5,
+        'Romania': 2, 
+        'Austria': 6
+        
+        }
+
+#upvote function
+def upvote():
+    
+
+total_votes_user1 =     
+
+#create and define multiple users
+class Users:
+    
+
+#sort the destinations as per rank results
+sorted_destinations_user1 = sorted(destinations_user1.items(), key=operator.itemgetter(1))
+
+print(sorted_destinations_user1)
+
+
+sorted_destinations_user2 = sorted(destinations_user2.items(), key=operator.itemgetter(1))
+
+print(sorted_destinations_user2)
